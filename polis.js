f3c34106a605d221972213e2f6d70b447e105d21
sureshdevaraj <suresh.k.devaraj@gmail.com>
2016-09-20 14:22:41 +0530

    var req = client.post("https://www98.verizon.com/Ondemand/vzWhatsHot.ashx", args, function (data, response) {

--- polis.js
+++ polis.js
@@ -11,11 +11,11 @@ var PORT = process.env.PORT || 9000;
 
 var router = express.Router(); 
 
-var headersInfo = { "Content-Type": "text/string" };
+var headersInfo = { "Content-Type": "application/json" };
 var Client = require('node-rest-client').Client;
 var client = new Client();
 var args = {
-  
+    "headers": headersInfo
 };
 
 router.post('/webhook', function (req, res) {
@@ -29,29 +29,36 @@ router.post('/webhook', function (req, res) {
 
      
 });
+router.get('/webhook', function (req, res) {
 
+    recommendTVNew(function (str) {
+        console.log("inside showrecommendation ");
+        res.json(recommendTVNew1(str));
+    });
+
+
+
+});
 function recommendTVNew(callback) {
 
-    var req = client.post("https://www98.verizon.com/Ondemand/api/utilWebAPI/GetWhatsHot", args, function (data, response) {
+    var req = client.post("https://www98.verizon.com/Ondemand/vzWhatsHot.ashx", args, function (data, response) {
 
-        console.log("recommendTVNew");
-        if (Buffer.isBuffer(data)) {
-            data = data.toString('utf8');
-        }
-
+        
+      
+        console.log("api resp:" + data);
  
         callback(data);
 
     });
-}
-function recommendTVNew1(apiresp) {
+}function recommendTVNew1(apiresp) {
+
     var objToJson = {};
     objToJson = apiresp;
 
     //var output = eval('(' + JSON.stringify(apiresp) + ')');
-    console.log("apiresp1:" + JSON.stringify(objToJson));
+    //console.log("apiresp1:" + JSON.stringify(objToJson));
     //console.log("output1:" + output);
-    var parsedResponse = JSON.parse(JSON.stringify(objToJson));
+    var parsedResponse = JSON.parse(apiresp);
 
 
     //console.log(aa);
@@ -65,16 +72,7 @@ function recommendTVNew1(apiresp) {
     });
 
 }
-router.get('/webhook', function (req, res) {
 
-    recommendTVNew(function (str) {
-        console.log("inside showrecommendation ");
-        res.json(recommendTVNew1(str));
-    });
-
-
-
-});
 
 router.get('/vzwhatshot', function (req, res) {
     var yourJson = "'facebook': { 'attachment': { 'type': 'template','payload': { 'template_type': 'generic','elements': [{'title': 'Shark Tank','subtitle': 'Shark Tank','image_url': 'http://image.vam.synacor.com.edgesuite.net/0f/07/0f07592094a2a596d2f6646271e9cb0311508415/w=414,h=303,crop=auto/?sig=88c390c980d4fa53d37ef16fbdc53ec3dfbad7d9fa626949827b76ae37140ac3&amp;app=powerplay','buttons': [    {'type': 'web_url','url': 'http://www.youtube.com/embed/SQ1W7RsXL3k','title': 'Watch video'    },    {'type': 'web_url','url': 'https://m.verizon.com/myverizonmobile/router.aspx?token=tvlisting','title': 'Record'    }]    },    {'title': 'Game of Thrones','subtitle': 'Game of Thrones','image_url': 'http://ia.media-imdb.com/images/M/MV5BMjM5OTQ1MTY5Nl5BMl5BanBnXkFtZTgwMjM3NzMxODE@._V1_UX182_CR0,0,182,268_AL_.jpg','buttons': [    {'type': 'web_url','url': 'https://www.youtube.com/watch?v=36q5NnL3uSM','title': 'Watch video'    },    {'type': 'web_url','url': 'https://m.verizon.com/myverizonmobile/router.aspx?token=tvlisting','title': 'Record'    }]    },    {'title': 'The Night Of','subtitle': 'The Night Of','image_url': 'http://ia.media-imdb.com/images/M/MV5BMjQyOTgxMDI0Nl5BMl5BanBnXkFtZTgwOTE4MzczOTE@._V1_UX182_CR0,0,182,268_AL_.jpg','buttons': [    {'type': 'web_url','url': 'https://www.youtube.com/watch?v=36q5NnL3uSM','title': 'Watch video'    },    {'type': 'web_url','url': 'https://m.verizon.com/myverizonmobile/router.aspx?token=tvlisting','title': 'Record'}]}]}}}";
