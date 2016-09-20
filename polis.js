6049891ac97b7ae26a27c3dedd795b4682981d25
sureshdevaraj <suresh.k.devaraj@gmail.com>
2016-09-20 21:24:46 +0530

    app.all('*', function (req, res) {     res.header('Access-Control-Allow-Origin', '*');     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');     res.header('Access-Control-Allow-Headers', 'Content-Type');
    });

--- polis.js
+++ polis.js
@@ -26,19 +26,13 @@ router.get('/webhook', function (req, res) {
    
 
 });
-
-
-app.all('/', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next();
+app.all('*', function (req, res) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    
 });
 
-router.all('/', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next();
-});
 
 router.get('/vzwhatshot1', function (req, res) {
     
