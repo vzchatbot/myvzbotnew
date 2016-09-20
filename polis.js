373f3813637c68505eaa6288cce957edd2f9bc3c
sureshdevaraj <suresh.k.devaraj@gmail.com>
2016-09-21 01:59:25 +0530

    working

--- polis.js
+++ polis.js
@@ -1,33 +1,31 @@
 
 var express = require('express');
-
 var request = require('request');
 var cors = require('cors');
-
 var http = require('https');
-
 var app = express();
-
 var PORT = process.env.PORT || 9000;
-
 var router = express.Router();
-
 app.use(cors())
 app.options('*', cors());
 
+router.get('/webhook', function (req, res) {
 
-router.get('/vzwhatshot2', function (req, res) {
+    var intent = req.body.result.metadata.intentName;
 
-    recommendTVNew(function (str) {
-        console.log("inside showrecommendation ");
-        res.json(recommendTVNew1(str));
-    });
+    switch (intent) {
 
-   
+        case "showrecommendation":
+          
+
+            recommendTVNew(function (str) {
+                console.log("inside showrecommendation ");
+                res.json(recommendTVNew1(str));
+            });
+
+    }   
 
 });
-
-
 
 function recommendTVNew(callback) {
     request.get(
@@ -40,21 +38,14 @@ function recommendTVNew(callback) {
     );
 }
 
-
 function recommendTVNew1(apiresp) {
     var objToJson = {};
     objToJson = apiresp;
-
-
-    //var output = eval('(' + JSON.stringify(apiresp) + ')');
+    
     console.log("apiresp1:" + JSON.stringify(objToJson));
     //console.log("output1:" + output);
     var parsedResponse = JSON.parse(apiresp);
-
-
-    //console.log(aa);
-    //return objToJson;
-
+    
     return ({
         speech: "Here are some recommendations for tonight",
         displayText: "TV recommendations",
