2fda28d973f586c3ab166e9780f88b7cb30ae6a4
sureshdevaraj <suresh.k.devaraj@gmail.com>
2016-09-20 11:16:33 +0530

    var headersInfo = { "Content-Type": "application/json" };

--- polis.js
+++ polis.js
@@ -13,14 +13,12 @@ var router = express.Router();
 
 router.post('/webhook', function (req, res) {
 
-                            
+    var headersInfo = { "Content-Type": "application/json" };
     var Client = require('node-rest-client').Client;
     var client = new Client();
     var args = {
-        "headers": headersInfo,
-        "data": JSON.stringify(reqData)
-    };
-        
+        "headers": headersInfo
+    }; 
     var req = client.post("https://www98.verizon.com/foryourhome/vzrepair/flowengine/vzwhatshot.ashx", args, function (data, response) {
 
         var parsedData = "";
