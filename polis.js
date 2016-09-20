c63e62eda918dbd689b3b38a8e0ff8d8ace141ba
sureshdevaraj <suresh.k.devaraj@gmail.com>
2016-09-20 17:31:17 +0530

    router.get('/vzwhatshot', function (req, res) {
        console.log("vzwhatshot");
    
        var req = client.post("https://www98.verizon.com/Ondemand/api/utilWebAPI/GetWhatsHot", args, function (data, response) {
    
            console.log("vzwhatshot1");
            res.json(data);
    
        });
    
    });

--- polis.js
+++ polis.js
@@ -18,25 +18,30 @@ var args = {
     "headers": headersInfo
 };
 
-router.post('/webhook', function (req, res) {
 
-    recommendTVNew(function (str) {
-        console.log("inside showrecommendation ");
-        res.json(recommendTVNew1(str));
+
+router.get('/webhook', function (req, res) {
+
+    var req1 = client.get("https://myvzbot.herokuapp.com/api/vzwhatshot", args, function (data, response) {
+
+        console.log("recommendTVNew");
+
+        res.on('data', function (chunk) {
+            res.json(data);
+            // process utf8 text chunk
+        });
+
     });
-
-
-
 
 });
 
 function recommendTVNew(callback) {
 
-    request('https://www98.verizon.com/Ondemand/vzWhatsHot.ashx', function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log("recommendTVNew");
-            callback(body);
-        }
+    var req = client.post("https://myvzbot.herokuapp.com/api/vzwhatshot", args, function (data, response) {
+
+        console.log("recommendTVNew");
+        callback(data);
+
     });
 
    
@@ -48,7 +53,7 @@ function recommendTVNew1(apiresp) {
     //var output = eval('(' + JSON.stringify(apiresp) + ')');
     console.log("apiresp1:" + JSON.stringify(objToJson));
     //console.log("output1:" + output);
-    var parsedResponse = JSON.parse(apiresp);
+   // var parsedResponse = JSON.parse(apiresp);
 
 
     //console.log(aa);
@@ -57,44 +62,26 @@ function recommendTVNew1(apiresp) {
     return ({
         speech: "Here are some recommendations for tonight",
         displayText: "TV recommendations",
-        data: parsedResponse,
+        data: apiresp,
         source: "Zero Service - app_zero.js"
     });
 
 }
-router.get('/webhook', function (req, res) {
 
-    recommendTVNew(function (str) {
-        console.log("inside showrecommendation ");
-        res.json(recommendTVNew1(str));
-    });
-
-
-
-});
 
 router.get('/vzwhatshot', function (req, res) {
-    var yourJson = "'facebook': { 'attachment': { 'type': 'template','payload': { 'template_type': 'generic','elements': [{'title': 'Shark Tank','subtitle': 'Shark Tank','image_url': 'http://image.vam.synacor.com.edgesuite.net/0f/07/0f07592094a2a596d2f6646271e9cb0311508415/w=414,h=303,crop=auto/?sig=88c390c980d4fa53d37ef16fbdc53ec3dfbad7d9fa626949827b76ae37140ac3&amp;app=powerplay','buttons': [    {'type': 'web_url','url': 'http://www.youtube.com/embed/SQ1W7RsXL3k','title': 'Watch video'    },    {'type': 'web_url','url': 'https://m.verizon.com/myverizonmobile/router.aspx?token=tvlisting','title': 'Record'    }]    },    {'title': 'Game of Thrones','subtitle': 'Game of Thrones','image_url': 'http://ia.media-imdb.com/images/M/MV5BMjM5OTQ1MTY5Nl5BMl5BanBnXkFtZTgwMjM3NzMxODE@._V1_UX182_CR0,0,182,268_AL_.jpg','buttons': [    {'type': 'web_url','url': 'https://www.youtube.com/watch?v=36q5NnL3uSM','title': 'Watch video'    },    {'type': 'web_url','url': 'https://m.verizon.com/myverizonmobile/router.aspx?token=tvlisting','title': 'Record'    }]    },    {'title': 'The Night Of','subtitle': 'The Night Of','image_url': 'http://ia.media-imdb.com/images/M/MV5BMjQyOTgxMDI0Nl5BMl5BanBnXkFtZTgwOTE4MzczOTE@._V1_UX182_CR0,0,182,268_AL_.jpg','buttons': [    {'type': 'web_url','url': 'https://www.youtube.com/watch?v=36q5NnL3uSM','title': 'Watch video'    },    {'type': 'web_url','url': 'https://m.verizon.com/myverizonmobile/router.aspx?token=tvlisting','title': 'Record'}]}]}}}";
-    console.log("vzwhatshot");
-    res.json(yourJson);
-
-});
-
-router.post('/vzwhatshot', function (req, res) {
 
     console.log("vzwhatshot");
 
-    var req = client.post("https://www98.verizon.com/ondemand/vzwhatshot.ashx", args, function (data, response) {
+    var req = client.post("https://www98.verizon.com/Ondemand/api/utilWebAPI/GetWhatsHot", args, function (data, response) {
 
         console.log("vzwhatshot1");
         res.json(data);
-        
 
     });
 
-   
-
 });
+
 
 
 
