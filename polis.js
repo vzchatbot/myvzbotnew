0534ff7ef7d553d62acce2db2fdf3103181bb1c7
sureshdevaraj <suresh.k.devaraj@gmail.com>
2016-09-21 01:50:08 +0530

    working

--- polis.js
+++ polis.js
@@ -18,24 +18,51 @@ app.options('*', cors());
 
 router.get('/vzwhatshot2', function (req, res) {
 
-    var options = {
-        host: 'www.verizon.com',
-        port: 443,
-        path: '/fiostv/myservices/admin/testwhatshot.ashx'
-    };
-
-    http.get(options, function (resp) {
-
-        resp.setEncoding('utf8');
-        resp.on('data', function (chunk) {
-
-            res.json(chunk);
-        });
+    recommendTVNew(function (str) {
+        console.log("inside showrecommendation ");
+        res.json(recommendTVNew1(str));
     });
+
+   
 
 });
 
 
+
+function recommendTVNew(callback) {
+    request.get(
+        'https://www.verizon.com/fiostv/myservices/admin/testwhatshot.ashx',
+        function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                callback(body);
+            }
+        }
+    );
+}
+
+
+function recommendTVNew1(apiresp) {
+    var objToJson = {};
+    objToJson = apiresp;
+
+
+    //var output = eval('(' + JSON.stringify(apiresp) + ')');
+    console.log("apiresp1:" + JSON.stringify(objToJson));
+    //console.log("output1:" + output);
+    var parsedResponse = JSON.parse(apiresp);
+
+
+    //console.log(aa);
+    //return objToJson;
+
+    return ({
+        speech: "Here are some recommendations for tonight",
+        displayText: "TV recommendations",
+        data: parsedResponse,
+        source: "Zero Service - app_zero.js"
+    });
+
+}
 
 // more routes for our API will happen here
 // REGISTER OUR ROUTES -------------------------------
