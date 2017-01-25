487c4960f99e970e954b15b494fc8a68f964a66b
vzchatbot <vzchatbot@hotmail.com>
2017-01-25 18:27:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var restify = require('restify');
 var builder = require('botbuilder');
+var request = require('request');
+var apiai = require('apiai');
 //=========================================================
 // Bot Setup
 //=========================================================
@@ -14,13 +16,93 @@ var connector = new builder.ChatConnector({
     appPassword: 'yq9ZmaxhNhbFJVh4AAP4gc6'
 });
 var bot = new builder.UniversalBot(connector);
+var APIAI_LANG = 'en' ;
+var APIAI_ACCESS_TOKEN = "c8021e1a2dac4f85aee8f805a5a920b2"; 
+var apiAiService = apiai(APIAI_ACCESS_TOKEN, {language: APIAI_LANG, requestSource: "fb"});
 server.post('/api/messages', connector.listen());
+
 //=========================================================
 // Bots Dialogs
 //=========================================================
-
+var headersInfo = { "Content-Type": "application/json" };
 bot.dialog('/', function (session) {
 	console.log("Start Bill Summary")
+	/*
 	    session.send("# BillSumary");	
 		session.send("Your bill amount is **$170** and due on **02/09/2017**");	
+		*/
+	 var apiaiRequest  = apiAiService.textRequest(text,{sessionId: sessionIds.get(sender)});
+        apiaiRequest .on('response', function (response)  {
+	 showBillInfo(response,session,function (str){ showBillInfoCallback(str,session)});
+	});
 });
+
+function showBillInfo(apireq, sender, callback) {
+    console.log("showBillInfo Called");
+    try {
+
+        var args = {
+            json: {
+                Flow: config.FlowName,
+                Request:
+                {
+                    ThisValue: 'BillInfo',
+                    BotProviderId: sender
+                }
+            }
+        };
+        console.log(" Request for showBillInfo json " + JSON.stringify(args));
+	
+        request.post({
+        url: 'https://www.verizon.com/fiostv/myservices/admin/botapinew.ashx',
+        proxy: '',
+        headers: headersInfo,
+        method: 'POST',
+        json: args.json
+        },
+            function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    callback(body);
+                }
+                else
+                    console.log(' error on callback for showBillInfo : ' + error + ' body: ' + JSON.stringify(body));
+            }
+        );
+    }
+    catch (experr) {
+        console.log('error on  showOutagetickets : ' + experr);
+    }
+   console.log("showOutagetickets completed");
+}
+
+function showBillInfoCallback(apiresp, usersession) {
+    var objToJson = {};
+    objToJson = apiresp;
+    var subflow = objToJson[0].Inputs.newTemp.Section.Inputs.Response;
+
+
+    logger.debug("showBillInfoCallback=" + JSON.stringify(subflow));
+    if (subflow != null
+        && subflow.facebook != null
+        && subflow.facebook.text != null && subflow.facebook.text == 'UserNotFound') {
+        console.log("showBillInfo subflow " + subflow.facebook.text);
+        var respobj = {
+            "facebook": {
+                "attachment": {
+                    "type": "template", "payload": {
+                        "template_type": "generic", "elements": [
+                            {
+                                "title": "You have to Login to Verizon to proceed", "image_url": config.vzImage, "buttons": [
+                                    { "type": "account_link", "url": config.AccountLink }]
+                            }]
+                    }
+                }
+            }
+        };
+
+        session.send(usersession, respobj.facebook);
+    }
+    else {
+        session.send(usersession, subflow.facebook);
+    }
+}
