fc4f4ed77e4fc2983359f1ab2872e9827b9b4b4f
vzchatbot <vzchatbot@hotmail.com>
2017-02-08 12:47:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -218,7 +218,7 @@ function stationsearchCallback(apiresp, senderid, userCoversationArr,session) {
 		var respobj = objToJson[0].Inputs.newTemp.Section.Inputs.Response;
 		logger.debug("Station Search Response " + JSON.stringify(respobj));
 		var msg ="{'facebook': { 'attachment': { 'type': 'template','payload': { 'template_type': 'generic','elements': [{'title': 'Shark Tank','subtitle': 'Shark Tank','image_url': 'http://image.vam.synacor.com.edgesuite.net/0f/07/0f07592094a2a596d2f6646271e9cb0311508415/w=414,h=303,crop=auto/?sig=88c390c980d4fa53d37ef16fbdc53ec3dfbad7d9fa626949827b76ae37140ac3&amp;app=powerplay','buttons': [    {'type': 'web_url','url': 'http://www.youtube.com/embed/SQ1W7RsXL3k','title': 'Watch video'    },    {'type': 'web_url','url': 'https://m.verizon.com/myverizonmobile/router.aspx?token=tvlisting','title': 'Record'    }]    },    {'title': 'Game of Thrones','subtitle': 'Game of Thrones','image_url': 'http://ia.media-imdb.com/images/M/MV5BMjM5OTQ1MTY5Nl5BMl5BanBnXkFtZTgwMjM3NzMxODE@._V1_UX182_CR0,0,182,268_AL_.jpg','buttons': [    {'type': 'web_url','url': 'https://www.youtube.com/watch?v=36q5NnL3uSM','title': 'Watch video'    },    {'type': 'web_url','url': 'https://m.verizon.com/myverizonmobile/router.aspx?token=tvlisting','title': 'Record'    }]    },    {'title': 'The Night Of','subtitle': 'The Night Of','image_url': 'http://ia.media-imdb.com/images/M/MV5BMjQyOTgxMDI0Nl5BMl5BanBnXkFtZTgwOTE4MzczOTE@._V1_UX182_CR0,0,182,268_AL_.jpg','buttons': [    {'type': 'web_url','url': 'https://www.youtube.com/watch?v=36q5NnL3uSM','title': 'Watch video'    },    {'type': 'web_url','url': 'https://m.verizon.com/myverizonmobile/router.aspx?token=tvlisting','title': 'Record'}]}]}}}}";
-                session.endDialog("attachments": [
+                session.endDialog({"attachments": [
     {
       "contentType": "application/vnd.microsoft.card.hero",
       "content": {
@@ -242,7 +242,7 @@ function stationsearchCallback(apiresp, senderid, userCoversationArr,session) {
         ]
       }
     }
-  ]);
+  ]});
 		
 		
 	}
