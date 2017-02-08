f07b43806695ce408d62bde9d0529dc61e1101d6
v892852 <suresh.k.devaraj@one.verizon.com>
2017-02-08 13:38:55 +0530

    create reply with Carousel AttachmentLayout

--- app.js
+++ app.js
@@ -209,7 +209,7 @@ function stationsearchCallback(apiresp, senderid, userCoversationArr,session) {
 	try {
 		
 		logger.debug("stationsearchCallback ");
-
+		
 		//userCoversationArr.ufdresdatetime = getDateTime();
 		//userCoversationArr.ufdTimeTaken = getsecondstaken('ufd', userCoversationArr.ufdreqdatetime, userCoversationArr.ufdresdatetime);
 		//userCoversationArr.ufdreq = 'passed';
@@ -217,6 +217,16 @@ function stationsearchCallback(apiresp, senderid, userCoversationArr,session) {
 		
 		var respobj = objToJson[0].Inputs.newTemp.Section.Inputs.Response;
 		logger.debug("Station Search Response " + JSON.stringify(respobj));
+		
+		var cards = getCardsAttachments();
+		
+		// create reply with Carousel AttachmentLayout
+		var reply = new builder.Message(session)
+        .attachmentLayout(builder.AttachmentLayout.carousel)
+        .attachments(cards);
+		
+		session.send(reply);
+		/*
 		   var msg = new builder.Message(session)
             .attachments([
                 new builder.ReceiptCard(session)
@@ -231,7 +241,7 @@ function stationsearchCallback(apiresp, senderid, userCoversationArr,session) {
                     
             ]);
                 session.endDialog(msg);
-		
+		*/
 		
 	}
     catch (experr) {
@@ -255,3 +265,50 @@ function printChatHistory(userCoversationArr) {
 	
 }
 
+function getCardsAttachments(session) {
+	return [
+		new builder.HeroCard(session)
+            .title('Azure Storage')
+            .subtitle('Offload the heavy lifting of data center management')
+            .text('Store and help protect your data. Get durable, highly available data storage across the globe and pay only for what you use.')
+            .images([
+			builder.CardImage.create(session, 'https://docs.microsoft.com/en-us/azure/storage/media/storage-introduction/storage-concepts.png')
+		])
+            .buttons([
+			builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/storage/', 'Learn More')
+		]),
+
+		new builder.ThumbnailCard(session)
+            .title('DocumentDB')
+            .subtitle('Blazing fast, planet-scale NoSQL')
+            .text('NoSQL service for highly available, globally distributed apps—take full advantage of SQL and JavaScript over document and key-value data without the hassles of on-premises or virtual machine-based cloud database options.')
+            .images([
+			builder.CardImage.create(session, 'https://docs.microsoft.com/en-us/azure/documentdb/media/documentdb-introduction/json-database-resources1.png')
+		])
+            .buttons([
+			builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/documentdb/', 'Learn More')
+		]),
+
+		new builder.HeroCard(session)
+            .title('Azure Functions')
+            .subtitle('Process events with a serverless code architecture')
+            .text('An event-based serverless compute experience to accelerate your development. It can scale based on demand and you pay only for the resources you consume.')
+            .images([
+			builder.CardImage.create(session, 'https://azurecomcdn.azureedge.net/cvt-5daae9212bb433ad0510fbfbff44121ac7c759adc284d7a43d60dbbf2358a07a/images/page/services/functions/01-develop.png')
+		])
+            .buttons([
+			builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/functions/', 'Learn More')
+		]),
+
+		new builder.ThumbnailCard(session)
+            .title('Cognitive Services')
+            .subtitle('Build powerful intelligence into your applications to enable natural and contextual interactions')
+            .text('Enable natural and contextual interaction with tools that augment users\' experiences using the power of machine-based intelligence. Tap into an ever-growing collection of powerful artificial intelligence algorithms for vision, speech, language, and knowledge.')
+            .images([
+			builder.CardImage.create(session, 'https://azurecomcdn.azureedge.net/cvt-68b530dac63f0ccae8466a2610289af04bdc67ee0bfbc2d5e526b8efd10af05a/images/page/services/cognitive-services/cognitive-services.png')
+		])
+            .buttons([
+			builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/cognitive-services/', 'Learn More')
+		])
+	];
+}
