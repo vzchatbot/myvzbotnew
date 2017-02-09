2447ea34fa458836cf3fbe375013c876c9e09518
v892852 <suresh.k.devaraj@one.verizon.com>
2017-02-09 12:21:49 +0530

    .attachmentLayout(builder.AttachmentLayout.carousel)

--- app.js
+++ app.js
@@ -325,6 +325,7 @@ function stationsearchCallback(apiresp, senderid, userCoversationArr,session) {
 		
 		// WORKING CODE
 		var msg = new builder.Message(session)
+		 .attachmentLayout(builder.AttachmentLayout.carousel)
             .attachments([
 			{
 				contentType: "application/vnd.microsoft.card.hero",
@@ -351,6 +352,64 @@ function stationsearchCallback(apiresp, senderid, userCoversationArr,session) {
 							type: "imBack",
 							title: "more",
 							value: "sushi,3+"
+						}
+					]
+				}
+			},
+			{
+				contentType: "application/vnd.microsoft.card.hero",
+				content: {
+					title: "Tenpura",
+					subtitle: "Japanese first-class vegitables.",
+					images: [
+						{
+							url: "https://mydeploy.azurewebsites.net/tenpura.jpg"
+						}
+					],
+					buttons: [
+						{
+							type: "imBack",
+							title: "1 piece",
+							value: "tenpura,1"
+						},
+						{
+							type: "imBack",
+							title: "2 piece",
+							value: "tenpura,2"
+						},
+						{
+							type: "imBack",
+							title: "more",
+							value: "tenpura,3+"
+						}
+					]
+				}
+			},
+			{
+				contentType: "application/vnd.microsoft.card.hero",
+				content: {
+					title: "Tofu",
+					subtitle: "Super healthy food condensed by soy milk.",
+					images: [
+						{
+							url: "https://mydeploy.azurewebsites.net/tofu.jpg"
+						}
+					],
+					buttons: [
+						{
+							type: "imBack",
+							title: "1 piece",
+							value: "tofu,1"
+						},
+						{
+							type: "imBack",
+							title: "2 piece",
+							value: "tofu,2"
+						},
+						{
+							type: "imBack",
+							title: "more",
+							value: "tofu,3+"
 						}
 					]
 				}
