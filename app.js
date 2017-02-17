fb4542c5b88e3a37de3082a3c6b4aa6cf7ae2d80
vzchatbot <vzchatbot@hotmail.com>
2017-02-17 15:39:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -103,32 +103,14 @@ server.get('/apipolling/', function (req, res) {
 
 });
 
-function parseCookies (request) {
-    var list = {},
-        rc = request.headers.cookie;
-
-    rc && rc.split(';').forEach(function( cookie ) {
-        var parts = cookie.split('=');
-        list[parts.shift().trim()] = decodeURI(parts.join('='));
-    });
-
-    return list;
-}
-http.createServer(function (request, response) {
-
-  // To Read a Cookie
-  var cookies = parseCookies(request);
-
-  // To Write a Cookie
-  response.writeHead(200, {
-    'Set-Cookie': 'mycookie=test',
-    'Content-Type': 'text/plain'
-  });
-  response.end('Hello World\n');
-}).listen(3978);
-
-console.log('Server running ' );
-
+var client = http.createClient(80, "google.com");
+request = client.request();
+request.on('response', function( res ) {
+    res.on('data', function( data ) {
+        console.log( data );
+    } );
+} );
+request.end();
 
 //=========================================================
 // Bots Dialogs
